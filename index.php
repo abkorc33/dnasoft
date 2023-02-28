c4ea88b6cb63ef03d6327808f65448fdbe9d702b
abkorc33 <abkorc33@gmail.com>
2023-02-28 13:58:05 +0900

    sortable_index

--- index.php
+++ index.php
@@ -5,66 +5,65 @@ include $_SERVER['DOCUMENT_ROOT']."/shkim/db.php";
 <head>
 	<meta charset="UTF-8">
 	<title>강의자료 게시판</title>
-	<link rel="stylesheet" type="text/css" href="/shkim/css/style.css" />
-	<link rel="stylesheet" type="text/css" href="/shkim/css/jquery-ui.css" />
-	<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js" ></script>
+	<link rel="stylesheet" type="text/css" href="./css/style.css" />
+	<link rel="stylesheet" type="text/css" href="./css/jquery-ui.css" />
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 	<script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" ></script>
+	<script type="text/javascript" src="/shkim/js/common.js"></script>
 	<script>
 	$(function() {
-		$("#sortable").sortable({
-			placeholder: "highlight", // 드래그 중인 아이템이 놓일 자리를 표시할 스타일을 지정
-			start: function(event, ui){	// 드래그 시작시 호출되는 이벤트 핸들러
-				// ui.item.data 아이템에 키, 값 쌍으로 데이터를 저장할 수 있다.
-				ui.item.data("start_pos", ui.item.index());	// 드래그 하는 아이템의 위치를 가져온다. 첫 번째 아이템은 0
-			},
-			stop: function(event, ui){	// 드랍하면 호출되는 이벤트 핸들러
-				let spos = ui.item.data("start_pos");	// 원래 게시글의 index값
-				let epos = ui.item.index();	// 드래그 드랍으로 변경된 index값
-			},
-			revert: true,
-			update: function(){
-				let reverse = $("#sortable tr").get().reverse();
-				let page = "<?=$page?>";
-				let pageNum2 = 5;
-				let pageNum1 = 1;
-				$(reverse).each(function(index){
-					if(page=="" || page==1){
-						$(this).children("#num").text(index+pageNum2);
-						$(this).children("input[type='number']").attr("name", index+pageNum2);
-					}else if(page==2){
-						$(this).children("#num").text(index+pageNum1);
-						$(this).children("input[type='number']").attr("name", index+pageNum1);
-					}
-					//document.querySelector('#name').innerHTML="현재담당자 :"+name+"<input type='hidden' id='uid' value="+uid+">";
-				});
-			
+		//	[ 페이지 내 게시글이 1개 이상일 경우 드래그 드롭 순서변경 가능 ] ============
+		let dragidx2 = $("#maxdragidx").val();
+		if(dragidx2 > 2){
+			$("#sortable").sortable({
+				placeholder: "highlight", // 드래그 중인 아이템이 놓일 자리를 표시할 스타일을 지정
+				revert: true,
+				//	[ 드래그가 끝난 후 실행되는 코드 ] ============
+				update: function(event, ui){
+					//	[ 페이지 내 가장 큰 dragidx ] ============
+					let dragidx = $("#maxdragidx").val();
+					//	[ 드래그가 끝난 후 dragidx desc 정렬 ] ============
+					$("#sortable tr").each(function(){
+						$(this).children("#num").text(dragidx);
+						dragidx = dragidx-1;
+					});
+					//	[ Ajax로 보낼 데이터 변수 정의 ] ============
+					let mode = "idxRow";
+					let myArray1 = [];	// idx 고유한 값
+					let myArray2 = [];	// 드래그로 변경된 dragidx 값
 
-				/*
-				let mode = "idxRow";
-				let dragRow = "input[name='input_check']:data-order".val();
-				$.ajax({
-					type:"POST",
-					url:"process.php",
-					data:{
-						dragRow: dragRow,
-						mode: mode,
-					},
-					dataType:"json",
-					success: function (data) {
-						if (data.result > 0) {
-							alert("순서가 변경되었습니다.");
+					//	[ Ajax로 보낼 데이터 추출 ] ============
+					$("#sortable tr td").each(function(){
+						let this_id = $(this).attr("data_idx");
+						if(this_id){
+							let this_id2 =$("[data_idx='"+this_id+"']").text();
+
+							myArray1.push(this_id.replace('num_', ''));
+							myArray2.push(this_id2);
 						}
-						else {
-							alert("순서 변경 처리중 에러가 발생했습니다.\n잠시후 다시 시도해 주세요.");
-						}
-					},
-					error: function (e) {
-						alert("순서 변경 실패");
-					}
-				});
-				*/
-			}
-		});
+					});
+					let page = "<?=$page?>";
+					$.ajax({
+						type:"POST",
+						url:"./page/board/process.php",
+						data:{
+							mode: mode,
+							myArray1: myArray1,
+							myArray2: myArray2,
+						},
+						success: function(data){
+							alert('index가 새롭게 정렬되었습니다.');
+							location.href = "http://192.168.3.8/shkim/index.php?page="+page;
+						},
+						error: function(status){
+							alert("순서 변경 실패");
+							console.log(status);
+						},
+					});
+				}
+			});
+		//	[ 페이지 내 게시글이 1일 경우 순서변경 불가능 ] ============
+		}else{}
 	});
 	</script>
 </head>
@@ -86,17 +85,18 @@ include $_SERVER['DOCUMENT_ROOT']."/shkim/db.php";
 		<div id="write_btn">
 			<a href="/shkim/page/board/write.php"><button class="btn">글쓰기</button></a>
 		</div>
-		<table class="list-table" id="sortable">
+		<table class="list-table">
 			<thead>
-			<tr>
+			<tr id='top'>
 				<th width="70">번호</th>
 				<th width="500">제목</th>
 				<th width="120">글쓴이</th>
 				<th width="100">작성일</th>
 				<th width="100">조회수</th>
-				<th width="100">추천수</th>
+                <th width="100">idx / 순번</th>
 			</tr>
 			</thead>
+			<tbody id="sortable">
 <?
 			if(isset($_GET['page'])){
 				$page = $_GET['page'];
@@ -118,19 +118,28 @@ include $_SERVER['DOCUMENT_ROOT']."/shkim/db.php";
 			$start_num = ($page-1) * $list; //시작번호 (page-1)에서 $list를 곱한다.
 			
 			if($page==1){
-				$i = 1;
+				$i = $row_num;
 			}else if($page==2){
-				$i = ($page * $page) + $page;
-			}else{
-				$i = (($page * $list) - $list) + 1;
-			}
-
+				$i = $row_num - $list;
+			}else if($page==3){
+				$i = $row_num - ($list*2);
+			}            
 			// board테이블에서 idx를 기준으로 내림차순해서 10개까지 표시
 			$sql2 = mq("select * from board order by dragidx desc limit $start_num, $list"); // 드래그로 바뀐 index순서로 적용하려면 order by dragidx
 
+			//	[ while 반복문 시작 ] ============
 			while($board = $sql2->fetch_array()){
-				//title변수에 DB에서 가져온 title을 선택
-				$title=$board["title"]; 
+				//	[ board테이블 필드 변수 정의 ] ============
+				$title      = $board["title"];
+				$dragidx    = $board["dragidx"]; 
+                if($maxdragidx < $dragidx){
+                    $maxdragidx = $dragidx;
+                }
+				$idx        = $board["idx"];
+				$name       = $board["name"]; 
+				$regdate    = $board["regdate"]; 
+				$hit        = $board["hit"]; 
+				$thumbup    = $board["thumbup"];
 				if(strlen($title)>30){ 
 					//title이 30을 넘어서면 ...표시
 					$title=str_replace($board["title"],mb_substr($board["title"],0,30,"utf-8")."...",$board["title"]);
@@ -138,16 +147,16 @@ include $_SERVER['DOCUMENT_ROOT']."/shkim/db.php";
 				//댓글 수 카운트
 				$sql3 = mq("select * from reply where con_num='".$board['idx']."'"); //reply테이블에서 con_num이 board의 idx와 같은 것을 선택
 				$rep_count = mysqli_num_rows($sql3); //num_rows로 정수형태로 출력
+
 ?>
-			<tbody>
 			<tr>
-				<td width="70" id="num" class="idxRow">
-				<?=$board['dragidx']?>				
+				<td width="70" id="num" data_idx="num_<?=$idx?>">
+				<?=$i?>
 				</td>
-				<td width="500" class="idxRow">
+				<td width="500">
 <?
 				$lockimg = "<img src='/shkim/img/lock.png' alt='lock' title='lock' with='20' height='20' />";
-				$boardtime = mb_substr($board['regdate'], 0, 10); //$boardtime변수에 board['regdate']값에서 날짜값만 넣음(시간 제외)
+				$boardtime = mb_substr($regdate, 0, 10); //$boardtime변수에 board['regdate']값에서 날짜값만 넣음(시간 제외)
 				$timenow = date("Y-m-d"); //$timenow변수에 현재 시간 Y-M-D를 넣음
 
 				if($boardtime==$timenow){
@@ -158,19 +167,24 @@ include $_SERVER['DOCUMENT_ROOT']."/shkim/db.php";
 
 				if($board['lock_post']=="1"){ 
 ?>
-				<a href='/shkim/page/board/ck_read.php?idx=<?php echo $board["idx"];?>'><?php echo $title, $lockimg;
+				<a href='/shkim/page/board/ck_read.php?idx=<?=$idx?>'><?php echo $title, $lockimg;
 				} else{ 
 ?>
-				<a href='/shkim/page/board/read.php?idx=<?=$board["idx"]; ?>'><?=$title; }?><span class="re_ct">[<?=$rep_count; ?>]<?=$img; ?></span></a></td>
-				<td width="120" class="idxRow"><?=$board['name']?></td>
-				<td width="100" class="idxRow"><?=$board['regdate']?></td>
-				<td width="100" class="idxRow"><?=$board['hit'];?></td>
-				<td width="100" class="idxRow"><?=$board['thumbup']?></td>
-			</tr>
-			</tbody>
+				<a href='/shkim/page/board/read.php?idx=<?=$idx ?>'><?=$title?><span class="re_ct">[<?=$rep_count ?>]<?=$img ?></span></a></td>
+				<td width="120"><?=$name?></td>
+				<td width="100"><?=$regdate?></td>
+				<td width="100"><?=$hit?></td>
+                <td width="100"><? echo "{$idx} / {$dragidx}" ?></td>
 <?
-			} 
+				}
 ?>
+			</tr>
+<?
+			$i--;
+			}
+			//	[ while 반복문 끝 ] ============
+?>
+			</tbody>
 		</table>
 
 		<!---페이징 넘버 --->
@@ -208,6 +222,8 @@ include $_SERVER['DOCUMENT_ROOT']."/shkim/db.php";
 			}
 ?>
 			</ul>
+			<!-- jQuery 에서 사용하기 위한 값 -->
+            <input type="hidden" id='maxdragidx' value="<?=$maxdragidx?>">
 		</div>
 	</div>
 </body>
--- page/board/process.php
+++ page/board/process.php
@@ -1,7 +1,4 @@
-<?php
-	include $_SERVER['DOCUMENT_ROOT']."/shkim/db.php";
-	require_once('../../password.php');
-?>
+<? include $_SERVER['DOCUMENT_ROOT']."/shkim/db.php"; ?>
 
 <link rel="stylesheet" type="text/css" href="/shkim/css/jquery-ui.css" />
 <link rel="stylesheet" type="text/css" href="/shkim/css/style.css" />
@@ -9,7 +6,7 @@
 <script type="text/javascript" src="/shkim/js/jquery-ui.js"></script>
 <script type="text/javascript" src="/shkim/js/common.js"></script>
 
-<?php
+<?
 // POST, GET방식으로 mode값 받음
 if(isset($_POST['mode'])){
 	$mode = $_POST['mode'];
@@ -18,14 +15,25 @@ else{
 	$mode = $_GET['mode'];
 }
 
+if($mode=='idxRow'){
+
+	$idx = $_POST[myArray1];
+	$dragidx = $_POST[myArray2];
+	
+	for($i=0; $i<=count($idx); $i++){
+		$sql = mq("update board set dragidx = '".$dragidx[$i]."' where idx = '".$idx[$i]."'");
+	}
+
+}
+
 //각 변수에 write.php에서 input name값들을 저장한다
 if($mode=='write'){
 	$username = $_POST['name'];
-	$userpw = password_hash($_POST['pw'], PASSWORD_DEFAULT);
+	$userpw = $_POST['pw'];
 	$title = $_POST['title'];
 	$content = $_POST['content'];
-	$date = date('Y-m-d');
-
+	$regdate = date('Y-m-d h:i:s');
+	$masteridx = $_POST['masteridx'];
 	// Auto increament 초기화
 	$mqq = mq("alter table board auto_increment =1");
 
@@ -41,8 +49,14 @@ if($mode=='write'){
 	$folder = "../../upload/".$filename;
 	move_uploaded_file($tmpfile,$folder);
 
-	if($username && $userpw && $title && $content){
-		$sql = mq("insert into board(name,pw,title,content,date,lock_post,file) values('".$username."','".$userpw."','".$title."','".$content."','".$date."','".$lo_post."','".$o_name."')");
+	$sql = mq("select max(`idx`) as drag from board");
+	$dragidx = $sql->fetch_array();
+	$dragidx = $dragidx['drag'] + 1;
+
+
+	// 패스워드 입력 생략
+	if($username && $title && $content){
+		$sql = mq("insert into board(name,title,content,regdate,lock_post,file,masteridx,dragidx) values('".$username."','".$title."','".$content."','".$regdate."','".$lo_post."','".$o_name."','".$masteridx."','".$dragidx."')");
 		echo "<script>
 		alert('글쓰기 완료되었습니다.');
 		location.href='/shkim/index.php';</script>";
@@ -51,6 +65,19 @@ if($mode=='write'){
 		alert('글쓰기에 실패했습니다.');
 		history.back();</script>";
 	}
+	/*
+	// 패스워드 입력부분 
+	if($username && $userpw && $title && $content){
+		$sql = mq("insert into board(name,pw,title,content,regdate,lock_post,file,masteridx) values('".$username."',password('".$userpw."'),'".$title."','".$content."','".$regdate."','".$lo_post."','".$o_name."','".$masteridx."')");
+		echo "<script>
+		alert('글쓰기 완료되었습니다.');
+		location.href='/shkim/index.php';</script>";
+	}else{
+		echo "<script>
+		alert('글쓰기에 실패했습니다.');
+		history.back();</script>";
+	}
+	*/
 
 }
 
@@ -61,13 +88,15 @@ if($mode=='modify'){
 	$board = $sql->fetch_array();
 
 	$username = $_POST['name'];
-	$userpw = password_hash($_POST['pw'], PASSWORD_DEFAULT);
+	$userpw = $_POST['pw'];
 	$title = $_POST['title'];
 	$content = $_POST['content'];
+	$editdate = date('Y-m-d h:i:s');
 
-	$sql1 = mq("update board set name='".$username."',pw='".$userpw."',title='".$title."',content='".$content."' where idx='".$bno."'");
+	$sql1 = mq("update board set name='".$username."',pw=password('".$userpw."'),title='".$title."',content='".$content."',editdate='".$editdate."' where idx='".$bno."'");
 	echo "<script>alert('글쓰기 완료되었습니다.'); location.href='/shkim/page/board/read.php?idx=$bno';</script>";
-
+	/*,pw=password('".$userpw."')
+	파일삭제 unlink()로 파일삭제*/
 }
 
 if($mode=='delete'){
@@ -82,11 +111,11 @@ if($mode=='delete'){
 if($mode=='reply_ok'){
 
 	$bno = $_GET['idx'];
-    $userpw = password_hash($_POST['dat_pw'], PASSWORD_DEFAULT);
+    $userpw = $_POST['dat_pw'];
 	$date = date('Y-m-d h:i');
     
     if($bno && $_POST['dat_user'] && $userpw && $_POST['content']){
-        $sql = mq("insert into reply(con_num,name,pw,content,date) values('".$bno."','".$_POST['dat_user']."','".$userpw."','".$_POST['content']."','".$date."')");
+        $sql = mq("insert into reply(con_num,name,pw,content,date) values('".$bno."','".$_POST['dat_user']."',password('".$userpw."'),'".$_POST['content']."','".$date."')");
         echo "<script>alert('댓글이 작성되었습니다.'); 
         location.href='/shkim/page/board/read.php?idx=$bno';</script>";
     }else{
@@ -97,51 +126,40 @@ if($mode=='reply_ok'){
 }
 
 if($mode=='rep_modify_ok'){
-
-	$rno = $_POST['rno'];//댓글번호
-	$sql = mq("select * from reply where idx='".$rno."'"); //reply테이블에서 idx가 rno변수에 저장된 값을 찾음
-	$reply = $sql->fetch_array();
-
-	$bno = $_POST['b_no']; //게시글 번호
-	$sql2 = mq("select * from board where idx='".$bno."'");//board테이블에서 idx가 bno변수에 저장된 값을 찾음
-	$board = $sql2->fetch_array();
-
 	$input_pw = $_POST['pw'];
-	$db_pw = $reply['pw'];
+	$rno = $_POST['rno'];//댓글번호
+	$bno = $_POST['b_no']; //input type hidden으로 넘겨받은 게시글 번호
+	$qry = "select * from reply where idx='{$rno}' and pw=password('{$input_pw}')";
+    $sql = mq($qry);//reply테이블에서 idx가 rno변수에 저장된 값을 찾음
+    $reply = $sql->fetch_array();
 
-	// reply 테이블의 idx가 rno변수에 저장된 값의 content를 선택해서 값 저장
 	// 수정시 비밀번호 체크
-	if (password_verify($input_pw, $db_pw)) {
-    $sql3 = mq("UPDATE reply SET content='" . $_POST['content'] . "' WHERE idx = '" . $rno . "'"); ?>
-    <script type="text/javascript">alert('수정되었습니다.');
-    location.replace("/shkim/page/board/read.php?idx=<?php echo $bno; ?>");
-    </script>
-    <?php
-    } else { ?>
-    <script type="text/javascript">alert('비밀번호가 틀립니다');
-    history.back();
-    </script>
-    <?php }
+	if ($reply['pw']) {
+    $sql3 = mq("UPDATE reply SET content='" . $_POST['content'] . "' WHERE idx = '{$rno}'"); ?>
+    <script type="text/javascript">alert('수정되었습니다.');location.replace("/shkim/page/board/read.php?idx=<?php echo $bno; ?>");</script>
+<? } else { ?>
+    <script type="text/javascript">alert('비밀번호가 틀립니다'); history.back(); </script>
+<? }
 }
 
 if($mode=='reply_delete'){
+    $input_pw   = $_POST['pw']; //사용자의 입력값을 받아온 pw
+	$rno = $_POST['rno']; 
+	$bno = $_POST['b_no']; // input type hidden으로 넘겨받은 게시글 번호
+    $qry = "select * from reply where idx='{$rno}' and pw=password('{$input_pw}')";
+    $sql = mq($qry); // $input_pw값을 암호화 한 sql문
+    $reply = $sql->fetch_array(); // $reply라는 변수에 배열의 키와 값을 담는다.
+    
+	// reply 테이블(qry가 reply테이블에 대한 쿼리니까 $reply에는 reply테이블 정보가 들어가는 것)에 무슨 값이 있는지 키와 값으로 보여준다.
+    print_r($reply);
 
-$rno = $_POST['rno']; 
-$sql = mq("select * from reply where idx='".$rno."'");//reply테이블에서 idx가 rno변수에 저장된 값을 찾음
-$reply = $sql->fetch_array();
+    if($reply['pw']) { // reply테이블에 해당 값이 있으면 함수 실행
+		// $sql = mq("delete from reply where idx='{$rno}'"); 중괄호{}로 감싸는 방법도 있다.
+        $sql = mq("delete from reply where idx='".$rno."'"); ?>
+        <script type="text/javascript">alert('댓글이 삭제되었습니다.'); location.replace("/shkim/page/board/read.php?idx=<?php echo $bno; ?>");</script>
+<?  }else{ ?>
+        <script type="text/javascript">alert('비밀번호가 틀립니다');history.back();</script>
+<?
+    }
 
-$bno = $_POST['b_no'];
-$sql2 = mq("select * from board where idx='".$bno."'");//board테이블에서 idx가 bno변수에 저장된 값을 찾음
-$board = $sql2->fetch_array();
-
-$input_pw = $_POST['pw'];
-$db_pw = $reply['pw'];
-
-if(password_verify($input_pw, $db_pw)) {
-	$sql = mq("delete from reply where idx='".$rno."'"); ?>
-	<script type="text/javascript">alert('댓글이 삭제되었습니다.'); location.replace("/shkim/page/board/read.php?idx=<?php echo $board["idx"]; ?>");</script>
-	<?php 
-	}else{ ?>
-		<script type="text/javascript">alert('비밀번호가 틀립니다');history.back();</script>
-	<?php }
 } ?>
