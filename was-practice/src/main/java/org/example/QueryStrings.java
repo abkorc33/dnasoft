76a8a896eed293dc2a66a824e54200c0f4b3a1f6
abkorc33 <abkorc@gmail.com>
2023-03-21 17:54:44 +0900

    ch04_04

--- was-practice/src/main/java/org/example/QueryStrings.java
+++ was-practice/src/main/java/org/example/QueryStrings.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class QueryStrings {
+public class QueryStrings { // 일급컬렉션
     private List<QueryString> queryStrings = new ArrayList<>();
 
     // "operand1=11  operator=*   operand2=55"
@@ -19,7 +19,8 @@ public class QueryStrings {
                     queryStrings.add(new QueryString(values[0], values[1]));
                 });
     }
-
+    // 여러 개의 QueryString이 있는 경우 각각의 key에 대해 존재하는지 filter를 통해 먼저 거르고
+    // 존재한다면 Value값을 가져와 첫번째 것을 반환하도록 하는 코드가 추가
     public String getValue(String key) {
         return this.queryStrings.stream()
                 .filter(queryString -> queryString.exists(key))
