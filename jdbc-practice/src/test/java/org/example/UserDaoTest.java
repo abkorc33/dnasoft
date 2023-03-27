691ba09b5659ad71864186bf6b7beba3ba3ddbfb
abkorc33 <abkorc@gmail.com>
2023-03-27 11:03:29 +0900

    add_jdbc_practice

---
+++ jdbc-practice/src/test/java/org/example/UserDaoTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserDaoTest {
+
+    @BeforeEach // 테스트 코드를 실행하기에 앞서 수행해야하는 작업이 있는경우 해당 메소드에 작성하는 어노테이션
+    void setUp() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("db_schema.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
+    @Test
+    void createTest() throws SQLException {
+        UserDao userDao = new UserDao();
+
+        userDao.create(new User("wizard", "password", "name", "email"));
+
+        User user = userDao.findByUserId("wizard");
+        assertThat(user).isEqualTo(new User("wizard", "password", "name", "email"));
+    }
+}
